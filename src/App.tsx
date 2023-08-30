181ba5cc8598a6dc3df4ab9f655ca4ccdca41f25
dayannealcantara <dayannealcantara14@gmail.com>
2023-08-29 22:02:45 -0300

    fix: tweaks after eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import{BrowserRouter} from "react-router-dom"
-import { AppRoutes } from "./routes";
-import { AppThemeProvider } from "./shared/contexts";
+import{BrowserRouter} from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { AppThemeProvider } from './shared/contexts';
 
 
 export const App =()=> {
@@ -12,6 +12,6 @@ export const App =()=> {
     </AppThemeProvider>
   
   );
-}
+};
 
 
