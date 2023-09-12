8bbc602081a0fe84080af48df7114c83769b9b70
dayannealcantara <dayannealcantara14@gmail.com>
2023-09-11 21:59:11 -0300

    feat:passing properties to detail bar buttons

--- src/shared/components/ferramenta-de-detalhes/FerramentasDeDetalhes.tsx
+++ src/shared/components/ferramenta-de-detalhes/FerramentasDeDetalhes.tsx
@@ -1,16 +1,47 @@
 
 import { Box, Button, Paper, useTheme, Icon} from '@mui/material';
 
-export const FerramentasDeDetalhes : React.FC = () => {  
+
+interface IFerramentasDeDetalhesProps { 
+  textoBtn?: string;
+  btnNovo?: boolean; 
+  btnVoltar?: boolean; 
+  btnApagar?: boolean; 
+  btnSalvar?: boolean; 
+  btnSalvarFechar?: boolean; 
+  aoClicarNovo?: (()=> void) 
+  aoClicarVoltar?: (()=> void) 
+  aoClicarApagar?: (()=> void) 
+  aoClicarSalvar?: (()=> void) 
+  aoClicarSalvarFechar?: (()=> void) 
+}
+
+
+export const FerramentasDeDetalhes : React.FC<IFerramentasDeDetalhesProps > = ({textoBtn='Novo',
+  btnApagar=true, 
+  btnNovo=true, 
+  btnSalvar=true,
+  btnSalvarFechar=false, 
+  btnVoltar=true,
+  aoClicarApagar,
+  aoClicarNovo,
+  aoClicarSalvar,
+  aoClicarSalvarFechar,
+  aoClicarVoltar}) => {  
   const theme = useTheme();
 
   return (
-    <Box height={theme.spacing(5)} marginX={1} padding={1} paddingX={2} display="flex" alignItems="center" gap={1} component={Paper}>     
-      <Button variant='contained' disableElevation color='primary' endIcon={<Icon>save</Icon>}>Salvar</Button>
-      <Button variant='outlined' disableElevation color='primary' endIcon={<Icon>save</Icon>}>Salvar e Voltar</Button>
-      <Button variant='outlined' disableElevation color='primary' endIcon={<Icon>delete</Icon>}>Apagar</Button>
-      <Button variant='outlined' disableElevation color='primary' endIcon={<Icon>add</Icon>}>Novo</Button>   
-      <Button variant='outlined' disableElevation color='primary' endIcon={<Icon>arrow_back</Icon>}>Voltar</Button>     
+    <Box height={theme.spacing(5)} marginX={1} padding={1} paddingX={2} display="flex" alignItems="center" gap={1} component={Paper}> 
+        
+      {btnSalvar &&(<Button variant='contained' disableElevation color='primary' endIcon={<Icon>save</Icon>} onClick={aoClicarSalvar}>Salvar</Button>)}
+
+      {btnSalvarFechar && (<Button variant='outlined' disableElevation color='primary' endIcon={<Icon>save</Icon>} onClick={aoClicarSalvarFechar}>Salvar e Voltar</Button>)}
+
+      {btnApagar &&(<Button variant='outlined' disableElevation color='primary' endIcon={<Icon>delete</Icon>} onClick={aoClicarApagar}>Apagar</Button>)}
+
+      {btnNovo &&(<Button variant='outlined' disableElevation color='primary' endIcon={<Icon>add</Icon>} onClick={aoClicarNovo}>{textoBtn}</Button>)} 
+
+      {btnVoltar &&(<Button variant='outlined' disableElevation color='primary' endIcon={<Icon>arrow_back</Icon>} onClick={aoClicarVoltar}>Voltar</Button>)}  
     </Box>
   );
 };
