f07f42d7392fabc24e1afd958f2b09a877059d71
dayannealcantara <dayannealcantara14@gmail.com>
2023-09-13 21:06:19 -0300

    feat:setting some environment variables

--- src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
+++ src/shared/components/barra-de-ferramentas/BarraDeFerramentas.tsx
@@ -1,5 +1,6 @@
 
 import { Box, Button, Paper, TextField, useTheme, Icon } from '@mui/material';
+import { Environment } from '../../environment';
 
 
 interface IBarraDeFerramentasProps {
@@ -24,7 +25,8 @@ export const BarraDeFerramentas : React.FC<IBarraDeFerramentasProps> = ({textoDa
     <Box height={theme.spacing(5)} marginX={1} padding={1} paddingX={2} display="flex" alignItems="center" gap={1} component={Paper}>
 
       {mostrarInputBusca &&
-      (<TextField size='small' placeholder='Pesquisar...' value={textoDaBusca} onChange={(e)=> aoMudarBusca?.(e.target.value)}/>)
+      (<TextField size='small' placeholder={Environment.INPUT_DE_BUSCA}
+        value={textoDaBusca} onChange={(e)=> aoMudarBusca?.(e.target.value)}/>)
       }
 
       <Box flex={1} display="flex" justifyContent="end"> 
