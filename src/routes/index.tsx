e267ec33b2c940a2f38d0076df2ce90459ee969f
dayannealcantara <dayannealcantara14@gmail.com>
2023-09-13 22:20:57 -0300

    feat: inserting route to city listing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 
 import { useDrawerContext } from '../shared/contexts';
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDeCidade } from '../pages';
 
 export const AppRoutes = () => {
   const {  setDrawerOptions } = useDrawerContext();
@@ -15,12 +15,19 @@ export const AppRoutes = () => {
         path: '/pagina-inicial',
         label: 'Página inicial',
       },
+      {
+        icon: 'location_city',
+        path: '/cidades',
+        label: 'Cidades',
+      },
     ]);
   }, []);
 
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard/>} />
+      <Route path="/cidades" element={<ListagemDeCidade/>} />
+      {/* <Route path="/cidades/detalhes/:id" element={<ListagemDeCidade/>} /> */}
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
