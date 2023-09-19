393565fa258393b239b6a913bb5fe028a6fc2708
dayannealcantara <dayannealcantara14@gmail.com>
2023-09-18 22:26:01 -0300

    fix: adding data to mock

--- src/shared/service/api/pessoas/PessoasService.ts
+++ src/shared/service/api/pessoas/PessoasService.ts
@@ -2,21 +2,21 @@ import { Environment } from '../../../environment';
 import { Api } from '../axios-config';
 
 
-interface IListagemPessoa {
+export interface IListagemPessoa {
   id: number;
   email: string;
   cidadeId: number;
   nomeCompleto: string;
 }
 
-interface IDetalhePessoa {
+export interface IDetalhePessoa {
   id: number;
   email: string;
   cidadeId: number;
   nomeCompleto: string;
 }
 
-type TPessoasComTotalCount = {
+export type TPessoasComTotalCount = {
   data: IListagemPessoa[];
   totalCount: number;
 }
