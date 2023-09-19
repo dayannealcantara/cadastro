c7b2e88f8332831c80964cbaebf173243829745e
dayannealcantara <dayannealcantara14@gmail.com>
2023-09-18 22:25:33 -0300

    feat: developing table component

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -2,27 +2,35 @@
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { BarraDeFerramentas } from '../../shared/components/barra-de-ferramentas/BarraDeFerramentas';
 import { useSearchParams } from 'react-router-dom';
-import { useEffect, useMemo } from 'react';
-import { PessoasService } from '../../shared/service/api/pessoas/PessoasService';
+import { useEffect, useMemo, useState } from 'react';
+import { IListagemPessoa, PessoasService } from '../../shared/service/api/pessoas/PessoasService';
 import { useDebounce } from '../../shared/hooks';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 export const ListagemDePessoas: React.FC =() =>{
   const[searchParams, setSearchParams] = useSearchParams();
   const {debounce} = useDebounce(3000);
+
+  const [row, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setLoading] = useState(true);
 
   const busca = useMemo(()=>{
     return searchParams.get('busca') || '';
   },[searchParams]);
 
   useEffect(()=>{
+    setLoading(true);
     debounce(() => {
       PessoasService.getAll(1,busca)
         .then((result) => {
+          setLoading(false);
           if (result instanceof Error) {
             alert(result.message);
           } else { 
             console.log(result);
-          }
+            setRows(result.data);
+            setTotalCount(result.totalCount);          }
         });
     });
   },[busca]);
@@ -35,7 +43,27 @@ export const ListagemDePessoas: React.FC =() =>{
     <LayoutBaseDePagina titulo='Listagem de Pessoas' barraDeFerramentas={(<BarraDeFerramentas textoBtn='Nova' mostrarInputBusca
       textoDaBusca={busca}
       aoMudarBusca={texto => setSearchParams({busca: texto}, {replace:true})}
-    />)}>Testando</LayoutBaseDePagina>
+    />)}>
+      <TableContainer component={Paper} variant='outlined' sx={{m:1, width:'auto'}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {row.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>      
+    </LayoutBaseDePagina>
   );
-
 };
