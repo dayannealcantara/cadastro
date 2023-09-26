372a313fb62a8e16ea4f0512ca2314a2a8249a80
dayannealcantara <dayannealcantara14@gmail.com>
2023-09-25 21:55:50 -0300

    feat? inserting loading and person deletion function

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,6 +1,8 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import {  FerramentasDeDetalhes } from '../../shared/components';
+import { useEffect, useState } from 'react';
+import { PessoasService } from '../../shared/service/api/pessoas/PessoasService';
 
 
 
@@ -8,25 +10,58 @@ export const DetalheDePessoas: React.FC =() =>{
   const {id = 'nova'} =useParams<'id'>();
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+
+
+
+  useEffect(() => {
+    if(id !== 'nova') {
+      setIsLoading(true);
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setIsLoading(false);
+
+          if(result instanceof Error) {
+            alert(result.message);
+            navigate('/pessoas');
+          } else {
+            setNome(result.nomeCompleto);
+            console.log(result);
+          }
+        }
+        );
+    }
+  }, [id]);
+
   const handleSave = () => {
     console.log('Save');
   };
-  const handleDelete = () => {
-    console.log('Delete');
+  const handleDelete =(id:number) =>{
+    if(confirm('Deseja realemente excluir?')) {
+      PessoasService.deleteById(id)
+        .then(result => {
+          if(result instanceof Error){
+            alert(result.message);
+          } else {          
+            alert('Registro deletado com sucesso');
+            navigate('/pessoas');
+          }
+        });
+    }
   };
-
+  
   return(
-    <LayoutBaseDePagina titulo='Detalhe de Pessoa' barraDeFerramentas={<FerramentasDeDetalhes  
+    <LayoutBaseDePagina titulo={id === 'nova' ? 'Nova pessoa' : nome }barraDeFerramentas={<FerramentasDeDetalhes  
       textoBtn='Nova' 
       btnNovo = {id !== 'nova'}
       btnApagar ={id !== 'nova'}
       btnSalvarFechar    
-
       aoClicarNovo={() => navigate('/pessoas/detalhe/nova')}
       aoClicarSalvar={handleSave}
       aoClicarVoltar={() => navigate('/pessoas')}
       aoClicarSalvarFechar={()=> {}}
-      aoClicarApagar={handleDelete}
+      aoClicarApagar={()=> handleDelete(Number(id))}
     />
     }>  
       <p>Detalhe de pessoa {id}</p>        
