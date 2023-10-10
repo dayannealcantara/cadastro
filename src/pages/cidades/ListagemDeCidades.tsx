2369d9e00e28c3feea68bbbe540e6d729e39cb6b
dayannealcantara <dayannealcantara14@gmail.com>
2023-10-09 21:50:37 -0300

    creating Details page and list of cities

---
+++ src/pages/cidades/ListagemDeCidades.tsx
@@ -0,0 +1,126 @@
+import { useEffect, useMemo, useState } from 'react';
+import { Icon, IconButton, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { BarraDeFerramentas } from '../../shared/components';
+import { LayoutBaseDePagina } from '../../shared/layouts';
+import { useDebounce } from '../../shared/hooks';
+import { CidadesService, IListagemCidade } from '../../shared/service/api/cidades/CidadesService';
+import { Environment } from '../../shared/environment';
+
+
+export const ListagemDeCidades: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
+  const navigate = useNavigate();
+
+  const [rows, setRows] = useState<IListagemCidade[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
+
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1');
+  }, [searchParams]);
+
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    debounce(() => {
+      CidadesService.getAll(pagina, busca)
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+
+            setTotalCount(result.totalCount);
+            setRows(result.data);
+          }
+        });
+    });
+  }, [busca, pagina]);
+
+  const handleDelete = (id: number) => {
+    if (confirm('Realmente deseja apagar?')) {
+      CidadesService.deleteById(id)
+        .then(result => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            setRows(oldRows => [
+              ...oldRows.filter(oldRow => oldRow.id !== id),
+            ]);
+            alert('Registro apagado com sucesso!');
+          }
+        });
+    }
+  };
+
+  return(
+    <LayoutBaseDePagina titulo='Listagem de Cidades' barraDeFerramentas={(<BarraDeFerramentas      
+      mostrarInputBusca
+      textoDaBusca={busca}
+      textoBtn='Nova' 
+      aoClicarBtn={() => navigate('/cidades/detalhe/nova')}
+      aoMudarBusca={texto => setSearchParams({ busca: texto, pagina: '1' }, { replace: true })}
+    />)}>
+      <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell width={100}>Ações</TableCell>
+              <TableCell>Nome</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>
+                  <IconButton size="small" onClick={() => handleDelete(row.id)}>
+                    <Icon>delete</Icon>
+                  </IconButton>
+                  <IconButton size="small" onClick={()=>navigate(`/pessoas/detalhes/${row.id}`)}>
+                    <Icon>edit</Icon>
+                  </IconButton>
+                </TableCell>
+                <TableCell>{row.nome}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant='indeterminate' />
+                </TableCell>
+              </TableRow>
+            )}
+            {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHA) && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <Pagination
+                    page={pagina}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHA)}
+                    onChange={(_, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>     
+    </LayoutBaseDePagina>
+  );
+};
