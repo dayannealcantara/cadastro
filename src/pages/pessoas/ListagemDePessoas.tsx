73202696a2a5fc3b6c8ea15a9d6e7a3db4fbfe9a
dayannealcantara <dayannealcantara14@gmail.com>
2023-10-09 21:51:16 -0300

    fix: column width adjustment

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -78,7 +78,7 @@ export const ListagemDePessoas: React.FC =() =>{
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Ações</TableCell>
+              <TableCell width={100}>Ações</TableCell>
               <TableCell>Nome Completo</TableCell>
               <TableCell>Email</TableCell>
             </TableRow>
