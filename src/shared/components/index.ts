9cf67e14df6f79c0556d5e93dd389299e1267767
dayannealcantara <dayannealcantara14@gmail.com>
2023-10-12 15:19:02 -0300

    feat: developing login screen

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,4 @@
 export * from './menu-lateral/MenuLateral';
 export * from './barra-de-ferramentas/BarraDeFerramentas';
 export * from './ferramenta-de-detalhes/FerramentasDeDetalhes';
+export * from './login/Login';
